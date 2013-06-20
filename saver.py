9791ea0613a05c949e59fbaf1ad711b42af85c64
Pedro Tacla Yamada <tacla.yamada@gmail.com>
2013-06-20 16:03:10 -0300

    Added basic site recognition

--- saver.py
+++ saver.py
@@ -3,6 +3,18 @@ import time
 import reddit
 from reddit.objects import Redditor, Submission
 
+sites = ('imgur', 'youtube',)
+
+class saved():
+    def __init__(self, title = '', url = ''):
+        self.title = title
+        self.url = url
+        for s in sites:
+            if s in self.url:
+                self.site = s
+                return
+        self.site = 'unknown'
+
 def write_queue(queue):
     output_file = open('links', 'w')
     for link in queue:
@@ -21,7 +33,7 @@ def main():
 
     # read saved links from reddit
     try:
-        print 'Reading saved links...',
+        print 'Reading saved links...'
         for link in api.get_saved_links(10):
             queue.append(link)
             save_count += 1
@@ -32,14 +44,28 @@ def main():
 
     # write saved links to a file
     try:
-        print 'Writing links to file...',
+        print 'Writing links to file...'
         write_queue(queue)
     except:
         print '\nSomething went wrong... Sorry'
         raise
         return 2
+    finally:
+        print 'Recognized and wrote %d links.' % save_count
 
-    # analyse file and prompt user for download selections [TODO]
+    # analyse file and prompt user for download selections
+    l_by_sites = { 'imgur': 0, 'youtube': 0, 'unknown': 0,}
+    links_file = open('links', 'r')
+    lines = links_file.readlines()
+
+    for i in range(0, len(lines), 3):
+        link = saved(lines[i], lines[i+1])
+        l_by_sites[link.site] += 1
+
+    print 'I found',
+    for site, nl in l_by_sites.items():
+        print '%d %s links' % (nl, site),
+    links_file.close()
 
     return 0
 
